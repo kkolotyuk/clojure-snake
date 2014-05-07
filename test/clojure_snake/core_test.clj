993c47f3fcf20f2c6d48e5377756f09ca6c684a6
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2014-05-08 00:43:27 +0700

    Tests for move snake

--- test/clojure_snake/core_test.clj
+++ test/clojure_snake/core_test.clj
@@ -7,4 +7,14 @@
     (is (= (add-points [0 1] '(-5 3) [3 0]) [-2 4])))
 
   (testing "Point to screen rect"
-    (is (= (point-to-screen-rect [4 -2]) [40 -20 10 10]))))
+    (is (= (point-to-screen-rect [4 -2]) [40 -20 10 10])))
+
+  (testing "Move snake without grow"
+    (let [snake (create-snake)
+          moved-snake (assoc snake :body '([2 1]))]
+      (is (= (move snake) moved-snake))))
+
+  (testing "Move snake with grow"
+    (let [snake (create-snake)
+          moved-snake (assoc snake :body '([2 1] [1 1]))]
+      (is (= (move snake :grow) moved-snake)))))
