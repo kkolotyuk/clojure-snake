21af80dd309b2752bca963202050f34eb4127021
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2014-05-09 01:46:16 +0700

    Separate app to two namespaces

--- src/clojure_snake/core.clj
+++ src/clojure_snake/core.clj
@@ -2,66 +2,21 @@
   (:import (java.awt Color Dimension)
            (javax.swing JPanel JFrame Timer JOptionPane)
            (java.awt.event ActionListener KeyListener KeyEvent))
+  (:require [clojure-snake.game :refer :all])
   (:gen-class))
 
-(def width 75)
-(def height 50)
+
 (def point-size 10)
 (def turn-millis 75)
-(def win-length 5)
+
 (def dirs {KeyEvent/VK_LEFT  [-1 0]
            KeyEvent/VK_RIGHT [1 0]
            KeyEvent/VK_UP    [0 -1]
            KeyEvent/VK_DOWN  [0 1]})
 
-(defn add-points [& pts]
-  "Adds points together. You can use add-points to calculate
-   the new position of a moving game object."
-  (apply mapv + pts))
-
 (defn point-to-screen-rect [[x y]]
   "Converts a point in game space to a rectangle on the screen."
   (map (partial * point-size) [x y 1 1]))
-
-(defn create-snake []
-  {:body '([1 1])
-   :dir [1 0]
-   :type :snake
-   :color (Color. 15 160 70)})
-
-(defn create-apple []
-  {:location [(rand-int width) (rand-int height)]
-   :type :apple
-   :color (Color. 210 50 90)})
-
-(defn move [{:keys [body dir] :as snake} & [grow]]
-  "Returns new snake after one step. Use optional param grow to eat an apple"
-  (assoc snake :body (cons (add-points (first body) dir)
-                           (if grow
-                             body
-                             (butlast body)))))
-
-(defn win? [{body :body}]
-  (>= (count body) win-length))
-
-(defn- head-overlaps-body? [{[head & tail] :body}]
-  (contains? (set tail) head))
-
-(def lose? head-overlaps-body?)
-
-(defn eats? [{[head] :body} {apple :location}]
-  (= head apple))
-
-(defn turn [snake newdir]
-  (assoc snake :dir newdir))
-
-(defn reset-game [snake apple]
-  (dosync (ref-set apple (create-apple))
-          (ref-set snake (create-snake)))
-  nil)
-
-(defn update-direction [snake newdir]
-  (when newdir (dosync (alter snake turn newdir))))
 
 (defn update-positions [snake apple]
   (dosync
---
+++ src/clojure_snake/game.clj
@@ -0,0 +1,51 @@
+(ns clojure-snake.game
+    (:import (java.awt Color)))
+
+(def win-length 5)
+(def width 75)
+(def height 50)
+
+(defn add-points [& pts]
+  "Adds points together. You can use add-points to calculate
+   the new position of a moving game object."
+  (apply mapv + pts))
+
+(defn create-snake []
+  {:body '([1 1])
+   :dir [1 0]
+   :type :snake
+   :color (Color. 15 160 70)})
+
+(defn create-apple []
+  {:location [(rand-int width) (rand-int height)]
+   :type :apple
+   :color (Color. 210 50 90)})
+
+(defn move [{:keys [body dir] :as snake} & [grow]]
+  "Returns new snake after one step. Use optional param grow to eat an apple"
+  (assoc snake :body (cons (add-points (first body) dir)
+                           (if grow
+                             body
+                             (butlast body)))))
+
+(defn win? [{body :body}]
+  (>= (count body) win-length))
+
+(defn- head-overlaps-body? [{[head & tail] :body}]
+  (contains? (set tail) head))
+
+(def lose? head-overlaps-body?)
+
+(defn eats? [{[head] :body} {apple :location}]
+  (= head apple))
+
+(defn turn [snake newdir]
+  (assoc snake :dir newdir))
+
+(defn reset-game [snake apple]
+  (dosync (ref-set apple (create-apple))
+          (ref-set snake (create-snake)))
+  nil)
+
+(defn update-direction [snake newdir]
+  (when newdir (dosync (alter snake turn newdir))))
