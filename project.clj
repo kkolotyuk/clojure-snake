5fd14ae5eb8b765d53a2553a7a63687dfa5a02d5
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2014-05-09 01:35:47 +0700

    Remove useless dependency

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "Simple Swing snake game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot clojure-snake.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
