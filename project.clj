f276aa1c6996591a1af34a0aefa49dcad78b8e08
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2014-05-09 01:06:28 +0700

    First working version

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Simple Swing snake game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :main ^:skip-aot clojure-snake.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojure_snake/core.clj
+++ src/clojure_snake/core.clj
@@ -1,12 +1,20 @@
 (ns clojure-snake.core
-  (:import (java.awt Color))
+  (:import (java.awt Color Dimension)
+           (javax.swing JPanel JFrame Timer JOptionPane)
+           (java.awt.event ActionListener KeyListener KeyEvent))
+  (:require [clojure.contrib import-static])
   (:gen-class))
+;; (import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
 
 (def width 75)
 (def height 50)
 (def point-size 10)
 (def turn-millis 75)
 (def win-length 5)
+(def dirs {KeyEvent/VK_LEFT  [-1 0]
+           KeyEvent/VK_RIGHT [1 0]
+           KeyEvent/VK_UP    [0 -1]
+           KeyEvent/VK_DOWN  [0 1]})
 
 (defn add-points [& pts]
   "Adds points together. You can use add-points to calculate
@@ -49,7 +57,80 @@
 (defn turn [snake newdir]
   (assoc snake :dir newdir))
 
+(defn reset-game [snake apple]
+  (dosync (ref-set apple (create-apple))
+          (ref-set snake (create-snake)))
+  nil)
+
+(defn update-direction [snake newdir]
+  (when newdir (dosync (alter snake turn newdir))))
+
+(defn update-positions [snake apple]
+  (dosync
+   (if (eats? @snake @apple)
+     (do (ref-set apple (create-apple))
+       (alter snake move :grow))
+     (alter snake move)))
+  nil)
+
+(defn fill-point [g pt color]
+  (let [[x y width height] (point-to-screen-rect pt)]
+    (.setColor g color)
+    (.fillRect g x y width height)))
+
+(defmulti paint (fn [g object & _] (:type object)))
+
+(defmethod paint :apple [g {:keys [location color]}]
+  (fill-point g location color))
+
+(defmethod paint :snake [g {:keys [body color]}]
+  (doseq [point body]
+    (fill-point g point color)))
+
+(defn game-panel [frame snake apple]
+  (proxy [JPanel ActionListener KeyListener] []
+    (paintComponent [g]
+      (proxy-super paintComponent g)
+      (paint g @snake)
+      (paint g @apple))
+    (actionPerformed [e]
+      (update-positions snake apple)
+      (when (lose? @snake)
+        (reset-game snake apple)
+        (JOptionPane/showMessageDialog frame "You lose!"))
+      (when (win? @snake)
+        (reset-game snake apple)
+        (JOptionPane/showMessageDialog frame "You win!"))
+      (.repaint this))
+    (keyPressed [e]
+      (update-direction snake (dirs (.getKeyCode e))))
+    (getPreferredSize []
+      (Dimension. (* (inc width) point-size)
+                  (* (inc height) point-size)))
+    (keyReleased [e])
+    (keyTyped [e])))
+
+
+(defn game []
+  (let [snake (ref (create-snake))
+        apple (ref (create-apple))
+        frame (JFrame. "Snake")
+        panel (game-panel frame snake apple)
+        timer (Timer. turn-millis panel)]
+    (doto panel
+      (.setFocusable true)
+      (.addKeyListener panel))
+    (doto frame
+      (.add panel)
+      (.pack)
+      (.setVisible true))
+    (.start timer)
+    [snake, apple, timer]))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (game))
+
+
+(game)
